5876288745412e0f107ec1bd7ecb16491bcb22f9
Stefan <stefantaitano@gmail.com>
2022-12-16 12:08:33 -0700

    add delete button

--- routes/stories.js
+++ routes/stories.js
@@ -48,22 +48,28 @@ router.get('/', ensureAuth, async (req, res) => {
 // @route GET /stories/edit/:id
 
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-  const story = await Story.findOne({ 
-    _id: req.params.id
-  }).lean()
-
-    if (!story) {
-      return res.render('error/404')
-    }
-
-    if (story.user != req.user.id) {
-      res.redirect('/stories')
-    } else {
-      res.render('stories/edit', {
-        story,
-      })
-    }
-
+  try {
+    const story = await Story.findOne({ 
+      _id: req.params.id
+    }).lean()
+  
+      if (!story) {
+        return res.render('error/404')
+      }
+  
+      if (story.user != req.user.id) {
+        res.redirect('/stories')
+      } else {
+        res.render('stories/edit', {
+          story,
+        })
+      }
+  
+  } catch (err) {
+    console.log(err)
+    return res.render('error'/500)
+  }
+  
 
 })
 
@@ -71,7 +77,8 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
 // @route PUT /stories/:id
 
 router.put('/:id', ensureAuth, async (req, res) => {
-  let story = await Story.findById(req.params.id).lean()
+  try {
+    let story = await Story.findById(req.params.id).lean()
 
   if(!story) {
     return res.render('error/404')
@@ -87,12 +94,26 @@ router.put('/:id', ensureAuth, async (req, res) => {
 
     res.redirect('/dashboard')
   }
+  } catch (err) {
+    console.log(err)
+    return res.render('error'/500)
+  }
+  
 
 
 })
 
-// @desc Process add form
-// @route POST /stories
+// @desc Delete story
+// @route DELETE /stories/:id
 
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({_id: req.params.id})
+    res.redirect('/dashboard')
+  } catch (err) {
+    console.log(err)
+    return res.render('error'/500)
+  }
+})
 
 module.exports = router
